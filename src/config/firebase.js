b8df6a4aad6243b656a37d0901cfb22f3a7cb462
Radoslav Nikolaev Yordanov <radoslavyordanov1415@gmail.com>
2026-02-27 18:01:51 +0100

    configure Firebase project with auth, Firestore and storage

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDOx5SUWAPedmpwYeMBpv-a4Jbqu9PonRM',
-  authDomain: 'moviewatchlist-67571.firebaseapp.com',
-  projectId: 'moviewatchlist-67571',
-  storageBucket: 'moviewatchlist-67571.firebasestorage.app',
-  messagingSenderId: '840892195421',
-  appId: '1:840892195421:web:9edfe7e2caec53bc2723b4',
+  apiKey: "AIzaSyDOx5SUWAPedmpwYeMBpv-a4Jbqu9PonRM",
+  authDomain: "moviewatchlist-67571.firebaseapp.com",
+  projectId: "moviewatchlist-67571",
+  storageBucket: "moviewatchlist-67571.firebasestorage.app",
+  messagingSenderId: "840892195421",
+  appId: "1:840892195421:web:9edfe7e2caec53bc2723b4",
 };
 
 const app = initializeApp(firebaseConfig);
